c4cb9349c5b2c5f802e8d590e30e266fc3e5e1a6
ekta111 <ekta.aitech@gmail.com>
2024-05-03 07:22:52 +0530

    validate token

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,11 +3,13 @@ import jwt from "jsonwebtoken";
 const verifyToken = (req, res, next) => {
     console.log("token verification initialized")
     const token = req.cookies["auth_token"];
+    console.log(token,"token found");
    
     if (!token) {
         return res.status(401).json({ message: "unauthorised" })
     }
     try {
+        console.log("trying to verify");
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
         req.userId = (decoded).userId;
         next();
