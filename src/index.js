909a4d20fdb09c7e9bf76517295bee5463f78654
ekta111 <ekta.aitech@gmail.com>
2024-05-20 08:12:20 +0530

    profile improvements

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ app.use("/auth", userAuth);
 app.use("/validate-token", verifyToken, async (req, res) => {
   const userInfo = await UserModel.findById(
     req.userId,
-    "name email roleType location verification status credits image"
+    "name email roleType location verification status credits image joinedAt"
   );
   return res
     .status(200)
