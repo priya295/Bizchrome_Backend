7a10afd6698f4d3abd832e18cf950830544d46b9
Godzilla19192 <amarthapa1919@gmail.com>
2024-05-26 10:24:42 +0530

    code updated

--- src/models/message.js
+++ src/models/message.js
@@ -4,6 +4,7 @@ import mongoose, { mongo } from "mongoose";
 const messageSchema = new mongoose.Schema({
   sender_id: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   content: {type: String, trim: true},
+  type: String,
   chat_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Chat'},
 },
 {timestamps: true}
